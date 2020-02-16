22c997d4f378365e9434a1e404cea3ee429c2857
Lucas Augusto Silva <luckaugusto@gmail.com>
2020-02-15 23:30:33 -0200

    Create About and Profile Pages, linking some pages too

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Lucas Augusto Silva</h1>
+    <h2>Programador Front-End</h2>
+    <p>
+      Analista de Sistemas na Ericsson Inovação, escrevo sobre Front-End, Lettering e Design...
+    </p>
+  </div>
+);
+
+export default Profile;
