ea2804cf55e6c182e42f58cfeb45e9269369e685
Lucas Augusto Silva <luckaugusto@gmail.com>
2020-03-15 23:36:04 -0300

    Styling Blog Post, and some improvments, adding the Commentary session

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    // needs to be the first to work with gatsby-remark-images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `uploads`,
+        path: `${__dirname}/static/assets/img`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,7 +33,25 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [],
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images-v2`,
+            options: {
+              name: `uploads`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 960,
+              linkImagesToOriginal: false,
+              loading: `lazy`,
+            },
+          },
+          `gatsby-remark-lazy-load`,
+          // needs to be the last plugin
+          `gatsby-remark-prismjs`,
+        ],
       },
     },
     `gatsby-transformer-sharp`,
